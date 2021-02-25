46c3e911b8a93f14a713ef44d78138a006699174
Tamil Selvan <tselvan@sqldba.net>
2021-02-26 10:57:53 +1300

    Update get_alwaysonavailabilitygroups_extendedeventdetails.sql

--- general/get_alwaysonavailabilitygroups_extendedeventdetails.sql
+++ general/get_alwaysonavailabilitygroups_extendedeventdetails.sql
@@ -1,12 +1,13 @@
 DECLARE @timeStr NVARCHAR(50) = CAST(SYSDATETIMEOFFSET() AS NVARCHAR(50));
 DECLARE @timeDiff INT=(SELECT CAST(REPLACE(REPLACE(RIGHT(@timeStr, 6),'+',''),':00','') AS INT));
-DECLARE @days INT=2;
+PRINT @timeDiff;
+DECLARE @hours INT=24;
 WITH cte_HADR
 AS (
 	SELECT object_name
 		,CONVERT(XML, event_data) AS data
 	FROM sys.fn_xe_file_target_read_file('AlwaysOn*.xel', NULL, NULL, NULL)
-		WHERE object_name != 'hadr_db_partner_set_sync_state'
+		--WHERE object_name != 'hadr_db_partner_set_sync_state'
 	)
 SELECT object_name
 	,DATEADD( hour,@timeDiff,data.value('(/event/@timestamp)[1]', 'datetime')) AS [timestamp] --NZT
@@ -17,6 +18,7 @@ SELECT object_name
 	,data.value('(/event/data[@name=''availability_replica_name''])[1]', 'varchar(max)') AS availability_replica_name
 	,data.value('(/event/data[@name=''message''])[1]', 'varchar(max)') AS [message]
 FROM cte_HADR
-WHERE data.value('(/event/@timestamp)[1]', 'datetime') > getdate() - @days
-and object_name like 'availability_replica%'
+WHERE DATEADD( hour,@timeDiff,data.value('(/event/@timestamp)[1]', 'datetime')) >= dateadd(hour,-@hours,getdate())
+--and object_name like 'availability_replica%' 
+--and data.value('(/event/data[@name=''availability_group_name''])[1]', 'varchar(max)')=''
 ORDER BY data.value('(/event/@timestamp)[1]', 'datetime') ASC;
